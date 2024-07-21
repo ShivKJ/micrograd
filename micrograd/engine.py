6f70ce2476ab3f7352aa138d01d8146eadfea125
ShivKJ <shivkj001@gmail.com>
2024-07-21 22:19:01 +0530

    updating micrograd

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,94 +1,102 @@
+from collections import deque
+from typing import Self
+
 
 class Value:
-    """ stores a single scalar value and its gradient """
-
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data: float, parents: tuple[Self, ...] = (), ops=''):
         self.data = data
         self.grad = 0
-        # internal variables used for autograd graph construction
-        self._backward = lambda: None
-        self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
-    def __add__(self, other):
+        self._backward = lambda: None
+        self._prev = set(parents)
+        self.ops = ops  # the op that produced this node, for graphviz / debugging / etc
+
+    def __add__(self, other: Self | float) -> Self:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
+
         out._backward = _backward
 
         return out
 
-    def __mul__(self, other):
+    def __mul__(self, other: Self | float) -> Self:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
+
         out._backward = _backward
 
         return out
 
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
+    def __pow__(self, p: int | float) -> Self:
+        assert isinstance(p, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** p, (self,), f'**{p}')
 
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+            self.grad += (p * self.data ** (p - 1)) * out.grad
+
         out._backward = _backward
 
         return out
 
-    def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+    def relu(self) -> Self:
+        out = Value(max(0., self.data), (self,), 'ReLU')
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+
         out._backward = _backward
 
         return out
 
-    def backward(self):
+    def backward(self, dx=1):
+        topo = deque()
+        visited_nodes = set()
 
-        # topological order all of the children in the graph
-        topo = []
-        visited = set()
-        def build_topo(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
-                topo.append(v)
+        def build_topo(node: Value):
+            if node not in visited_nodes:
+                visited_nodes.add(node)
+
+                for parent in node._prev:
+                    build_topo(parent)
+
+                topo.append(node)
+
         build_topo(self)
 
         # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
+        self.grad = dx
+
         for v in reversed(topo):
             v._backward()
 
-    def __neg__(self): # -self
+    def __neg__(self) -> Self:  # -self
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other: Self | float) -> Self:  # other + self
         return self + other
 
-    def __sub__(self, other): # self - other
+    def __sub__(self, other: Self | float) -> Self:  # self - other
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other: Self | float) -> Self:  # other - self
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other: Self | float) -> Self:  # other * self
         return self * other
 
-    def __truediv__(self, other): # self / other
-        return self * other**-1
+    def __truediv__(self, other: Self | float) -> Self:  # self / other
+        return self * other ** -1
 
-    def __rtruediv__(self, other): # other / self
-        return other * self**-1
+    def __rtruediv__(self, other: Self | float) -> Self:  # other / self
+        return other * self ** -1
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,54 +1,60 @@
 import random
+from abc import abstractmethod
+from operator import mul
+
 from micrograd.engine import Value
 
-class Module:
 
+class Module:
     def zero_grad(self):
         for p in self.parameters():
             p.grad = 0
 
-    def parameters(self):
-        return []
+    @abstractmethod
+    def parameters(self) -> list[Value]:
+        pass
+
 
 class Neuron(Module):
-
-    def __init__(self, nin, nonlin=True):
-        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+    def __init__(self, nin: int, nonlin=True):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
         self.b = Value(0)
         self.nonlin = nonlin
 
-    def __call__(self, x):
-        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+    def __call__(self, x: list[float]) -> Value:
+        act = sum(map(mul, self.w, x)) + self.b
+
         return act.relu() if self.nonlin else act
 
-    def parameters(self):
+    def parameters(self) -> list[Value]:
         return self.w + [self.b]
 
     def __repr__(self):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
-class Layer(Module):
 
-    def __init__(self, nin, nout, **kwargs):
+class Layer(Module):
+    def __init__(self, nin: int, nout: int, **kwargs):
         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
-    def __call__(self, x):
+    def __call__(self, x: list[float]):
         out = [n(x) for n in self.neurons]
         return out[0] if len(out) == 1 else out
 
-    def parameters(self):
+    def parameters(self) -> list[Value]:
         return [p for n in self.neurons for p in n.parameters()]
 
     def __repr__(self):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
+
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
+    def __init__(self, nin: int, nouts: list[int]):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i + 1], nonlin=i != len(nouts) - 1) for i in range(len(nouts))]
 
-    def __call__(self, x):
+    def __call__(self, x: list[float]) -> list[float]:
         for layer in self.layers:
             x = layer(x)
         return x
